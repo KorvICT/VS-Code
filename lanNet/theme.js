a822be65e17da9202b99d6c2edab55f0639f9923
KorvICT <xaviersgendron@gmail.com>
2023-01-16 19:17:51 -0800

    theme.js upd

--- lanNet/theme.js
+++ lanNet/theme.js
@@ -1,13 +1,13 @@
 // Elements
 const body = document.body;
-const darkButton = document.getElementById("darkb");
-const lightButton = document.getElementById("lightb");
+const darkButton = document.getElementById('darkb');
+const lightButton = document.getElementById('lightb');
 
 // Events
 darkButton.onclick = () => {
-    body.classList.replace("light","dark");
+    body.classList.replace('light','dark');
 }
 
 lightButton.onclick = () => {
-    body.classList.replace("dark","light");
+    body.classList.replace('dark','light');
 }
