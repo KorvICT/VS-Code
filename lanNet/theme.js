eb8c31b4fa4bf7a33324b74390cde72f55bed69c
KorvICT <xaviersgendron@gmail.com>
2023-02-13 20:56:21 -0800

    test 2

--- lanNet/theme.js
+++ lanNet/theme.js
@@ -2,11 +2,9 @@ const themeSelector = document.getElementById('theme-selector');
 const body = document.querySelector('body');
 
 themeSelector.addEventListener('change', () => {
-  if (themeSelector.value === 'theme-dark') {
-    body.classList.add('theme-dark');
-    body.classList.remove('theme-light');
-  } else if (themeSelector.value === 'theme-light') {
-    body.classList.add('theme-light');
-    body.classList.remove('theme-dark');
+  if (themeSelector.value === 'dark') {
+    body.classList.replace('light', 'dark');
+  } else if (themeSelector.value === 'light') {
+    body.classList.replace('dark', 'light')
   }
 });
