3527b0732147097bd5217db2b184081763807e5f
KorvICT <xaviersgendron@gmail.com>
2023-03-09 20:00:09 -0800

    theme selector attempt

--- lanNet/theme.js
+++ lanNet/theme.js
@@ -2,13 +2,15 @@ const bodyElement = document.querySelector('body');
 const lightThemeButton = document.querySelector('#light-button');
 const darkThemeButton = document.querySelector('#dark-button');
 
-lightThemeButton.addEventListener('click', () => {
-  bodyElement.classList.remove('dark');
-  bodyElement.classList.add('light');
-});
+lightThemeButton.addEventListener('click', theme, false);
 
-darkThemeButton.addEventListener('click', () => {
-  bodyElement.classList.remove('light');
-  bodyElement.classList.add('dark');
-});
+darkThemeButton.addEventListener('click', theme, false);
 
+function theme() {
+  if (bodyElement.classList.contains('dark')){
+    bodyElement.classList.replace('dark','light');
+  }
+  if (bodyElement.classList.contains('light')){
+    bodyElement.classList.replace('light','dark');
+  }
+}
